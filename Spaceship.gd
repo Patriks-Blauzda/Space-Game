a247685a4d2e578585a2eaf110706da19544adac
Patriks Blauzda <pblauzda@gmail.com>
2022-06-07 09:53:10 +0300

    commented current code

--- Spaceship.gd
+++ Spaceship.gd
@@ -1,19 +1,25 @@
 extends KinematicBody
 
+# normal movement speed, boost speed and acceleration
 export var spd = 150
 export var boost = 300
 export var accel = 5
 
 var currentspd = 0
+# to be used for the 3d matrix and multiplied by current speed
 var vel = Vector3.ZERO
-export var sens = 0.1
 
 
+# handles player movement and rotation
 func motion():
+	# takes joystick or arrow key input and turns it into a 2d vector
 	var direction = Input.get_vector("joystick_left", "joystick_right", "joystick_up", "joystick_down")
 	
+	# function rotates the player by specified angle (0.05 radians)
 	rotate_object_local(Vector3(direction.y, 0, direction.x), 0.05)
 	
+	
+	# if button is held, max speed is increased
 	if Input.is_action_pressed("throttle"):
 		if currentspd < boost:
 			currentspd += accel
@@ -23,14 +29,20 @@ func motion():
 		elif currentspd > spd:
 			currentspd -= accel
 	
+	# slows the player
 	if Input.is_action_pressed("brake"):
 		currentspd -= accel * 2
 	
+	# keeps speed above normal speed divided by 2, and below boost speed
 	currentspd = clamp(currentspd, spd/2, boost)
 	
+	# gets local Z axis (forward/backward) and multiplies it by the current speed
 	vel = transform.basis.z * currentspd
 	
+	# function returns a normalized and orthogonal 3d matrix
+	# used for preventing issues with rotation
 	transform = transform.orthonormalized()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -39,14 +51,16 @@ func _ready():
 
 
 func _physics_process(delta):
+	# used to display speed on the screen
 	$Label.text = str(currentspd)
 	
 	motion()
 	
+	# calculates collision and movement
 	var collisions = move_and_collide(vel * delta)
 
 
-
+# checks if ESC or R are pressed, esc toggles mouse mode and R reloads the game
 func _input(event):
 	if event is InputEventKey:
 		if event.scancode == KEY_ESCAPE && !event.echo && event.pressed:
